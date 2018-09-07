2c199110af41cf76c97b246381ad0fe1e8d96494
Joshua Puetz <josh@grorichpuetz.com>
2018-09-07 10:54:12 -0500

    Set up Aruba for testing

--- spec/codeship_migrate_to_github_app_spec.rb
+++ spec/codeship_migrate_to_github_app_spec.rb
@@ -1,13 +1,17 @@
+require 'spec_helper'
+
 RSpec.describe CodeshipMigrateToGithubApp do
   it "has a version number" do
     expect(CodeshipMigrateToGithubApp::VERSION).not_to be nil
   end
 end
 
-RSpec.describe CodeshipMigrateToGithubApp::CLI do
-  let(:run) { described_class.start }
+RSpec.describe CodeshipMigrateToGithubApp::CLI, :type => :aruba do
+  before(:each) { run('bin/codeship_migrate_to_github_app') }
+  let(:command) { find_command("bin/codeship_migrate_to_github_app") }
 
-  it "prints hello world" do
-    expect{run}.to output("Hello world").to_stdout
-  end
+  before(:each) { stop_all_commands }
+
+  it { expect(command).to be_successfully_executed }
+  it { expect(last_command_started.stdout).to eq "Hello world" }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "codeship_migrate_to_github_app"
+require "aruba/rspec"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
